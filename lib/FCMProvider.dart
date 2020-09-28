6af08e2fe91d630217c814f731c7b7b9ea01065d
Shady <ShadyZekry75@gmail.com>
2020-09-28 13:10:19 +0200

    Makes configuration functions dynamic

--- lib/FCMProvider.dart
+++ lib/FCMProvider.dart
@@ -4,7 +4,17 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:rxdart/rxdart.dart';
 
 class FCMProvider {
-  FCMProvider() {
+  final Function(Map<String, dynamic>) _launchTrayByMessage;
+  final Function(Map<String, dynamic>) _launchTrayByMessageIOS;
+  final Function(Map<String, dynamic>) _navigateByMessage;
+  final Function(Map<String, dynamic>) _navigateByMessageIOS;
+
+  FCMProvider(
+    this._launchTrayByMessage,
+    this._launchTrayByMessageIOS,
+    this._navigateByMessage,
+    this._navigateByMessageIOS,
+  ) {
     setFCMBackgroundMessageHandler();
   }
 
@@ -19,22 +29,26 @@ class FCMProvider {
     _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
         if (Platform.isIOS) {
-          _launchTrayByMessageiOS(message);
+          _launchTrayByMessageIOS(message);
           return;
         }
-        _launchTrayByMessage(message);
         print("onMessage: $message");
+
+        if (_launchTrayByMessage == null) return;
+        _launchTrayByMessage(message);
       },
       onLaunch: (Map<String, dynamic> message) async {
         print("onLaunch: $message");
       },
       onResume: (Map<String, dynamic> message) async {
         if (Platform.isIOS) {
-          _navigateByMessageiOS(message);
+          _navigateByMessageIOS(message);
           return;
         }
-        _navigateByMessage(message);
         print("onResume: $message");
+
+        if (_navigateByMessage == null) return;
+        _navigateByMessage(message);
       },
     );
     _firebaseMessaging.requestNotificationPermissions();
@@ -47,108 +61,6 @@ class FCMProvider {
       print('FCM NEW TOKEN : $token');
       fcmTokenSubject.add(token);
     });
-  }
-
-  ///For foreground messages
-  void _launchTrayByMessage(Map<String, dynamic> message) {
-    String target = message['data']['target'];
-
-    switch (target) {
-      case 'room':
-        String roomId = message['data']['room_id'];
-        String tag = 'room';
-        String body = message['notification']['body'];
-        trayNotificationSubject.add(InAppNotificationLaunched(
-            {'tag': tag, 'id': roomId, 'body': body}));
-        break;
-
-      case 'request':
-        String requestID = message['data']['request_id'];
-        String tag = 'request';
-        String body = message['notification']['body'];
-        trayNotificationSubject.add(InAppNotificationLaunched(
-            {'tag': tag, 'id': requestID, 'body': body}));
-        break;
-
-      case 'billNote':
-        String orderId = message['data']['order_id'];
-        String tag = 'billNote';
-        trayNotificationSubject
-            .add(InAppNotificationLaunched({'tag': tag, 'id': orderId}));
-        break;
-    }
-  }
-
-  void _launchTrayByMessageiOS(Map<String, dynamic> message) {
-    String target = message['target'];
-
-    switch (target) {
-      case 'room':
-        String roomId = message['room_id'];
-        String tag = 'room';
-        String body = message['aps']['alert']['body'];
-        trayNotificationSubject.add(InAppNotificationLaunched(
-            {'tag': tag, 'id': roomId, 'body': body}));
-
-        break;
-
-      case 'request':
-        String requestID = message['request_id'];
-        String tag = 'request';
-        String body = message['aps']['alert']['body'];
-        trayNotificationSubject.add(InAppNotificationLaunched(
-            {'tag': tag, 'id': requestID, 'body': body}));
-        break;
-    }
-  }
-
-  ///For background messages
-  void _navigateByMessage(Map<String, dynamic> message) {
-    String target = message['data']['target'];
-
-    switch (target) {
-      case 'room':
-        String roomId = message['data']['room_id'];
-        String tag = 'room';
-        notificationSubject.add({'tag': tag, 'id': roomId});
-        break;
-
-      case 'request':
-        String requestID = message['data']['request_id'];
-        String tag = 'request';
-        notificationSubject.add({'tag': tag, 'id': requestID});
-        break;
-
-      case 'order':
-        String orderId = message['data']['order_id'];
-        String tag = 'order';
-        notificationSubject.add({'tag': tag, 'id': orderId});
-        break;
-    }
-  }
-
-  void _navigateByMessageiOS(Map<String, dynamic> message) {
-    String target = message['target'];
-
-    switch (target) {
-      case 'room':
-        String roomId = message['room_id'];
-        String tag = 'room';
-        notificationSubject.add({'tag': tag, 'id': roomId});
-        break;
-
-      case 'request':
-        String requestID = message['request_id'];
-        String tag = 'request';
-        notificationSubject.add({'tag': tag, 'id': requestID});
-        break;
-
-      case 'order':
-        String orderId = message['order_id'];
-        String tag = 'order';
-        notificationSubject.add({'tag': tag, 'id': orderId});
-        break;
-    }
   }
 
   void cleanTokenListener() {
